e6cf4cc943727811331e46ae2e763cda031696bf
WASIF AL MAHMUD <wamvmaw@gmail.com>
2019-12-20 12:47:44 +0600

    initial client application to connect in server of specified ip and port

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String args[])
+    {
+        Client client = new Client("127.0.0.1", 5000);
+        System.out.println("hi");
+    }
+}
