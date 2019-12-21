0dd1d714e24537f5a85e558d7f4111d1359817e0
WASIF AL MAHMUD <wamvmaw@gmail.com>
2019-12-22 00:51:28 +0600

    fixed client application access to the server

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,9 +2,7 @@ package com.company;
 
 public class Main {
 
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         Client client = new Client("127.0.0.1", 5000);
-        System.out.println("hi");
     }
 }
